4892abcc56a23d67e6d3f28121f99939cb34aba9
Hans Micaya <hcmicaya@gmail.com>
2022-05-13 21:46:56 -0400

    add header and about us components

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import {images} from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nulla excepturi sint inventore impedit animi tempora, veritatis nihil voluptatem optio labore non hic perferendis, asperiores illo sit quas odit at. </p>
+      <button type="button" className="custom__button">Explore Menu </button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
